de33f91f772a2f0a252c90ca02493e28e5e4f75b
Karen Spriggs <k.spriggs01@gmail.com>
2022-11-24 20:53:47 -0600

    k

--- SearchAlgorithmDemo/SearchAlgorithmDemo/SortingAlgorithms.cs
+++ SearchAlgorithmDemo/SearchAlgorithmDemo/SortingAlgorithms.cs
@@ -28,29 +28,67 @@ namespace SearchAlgorithmDemo
 
         // Insertion sort has a worse case complexity of O(n^2) and a best case of O(n)
         // It works by separating the items that have been sorted from the ones that have not
+        // Once it finds an item in the list that has not been sorted, it goes to look for where the item
+        // should be placed relative to the items already sorted
         // 
         public static void InsertionSort(int[] data)
         {
-            for (int i = 1; i < data.Length; i++)
+            for (int i = 0; i < data.Length-1; i++)
             {
-                
+                for (int j = i-1; j > 0; j--)
+                {
+                    if (data[j]< data[j - 1])
+                    {
+                        int tempvalue = data[j];
+                        data[j-1] = data[j];
+                        data[j] = tempvalue;
+                    } else
+                    {
+                        return;
+                    }
+                }
             }
         }
 
         // Selection sort has an average and worst case complexity of O(n^2) and a best case of O(n)
         // It is bad with longer lists and more inefficient than insertion sort
-        // It finds the smallest item out of the non sorted elements and swaps it with the element
-        // after the latest element that was sorted
+        // It finds the smallest (or largest, depending on the sorting order) item out of the non sorted elements
+        // and swaps it with the element after the latest element that was sorted
+        // In my example I am finding the smallest element out of the non sorted elements
         public static void SelectionSort(int[] data)
         {
             for (int i = 0; i < data.Length; i++)
             {
+                int currentMinimum = data[i];
+                int minimumIndex = i;
 
+                for (int j = i+i; j<data.Length; j++)
+                {
+                    if (data[j] > currentMinimum)
+                    {
+                        currentMinimum = data[j];
+                        minimumIndex = j;
+                    }
+                }
+
+                if (currentMinimum != data[i])
+                {
+                    int tempvalue = data[i];
+                    data[i] = currentMinimum;
+                    data[minimumIndex] = tempvalue;
+                }
             }
         }
 
         // Heap sort has an average complexity of O(n log n) 
+        // Kind of like selectionsort but it keeps the unsorted values in a heap for faster element finding
+
         public static void HeapSort(int[] data)
+        {
+
+        }
+
+        public void MakeHeap(int[] data)
         {
 
         }
