c348f55cc6126bbc2ace021b4b68b713ae403d8a
Karen Spriggs <k.spriggs01@gmail.com>
2022-11-27 20:28:43 -0600

    tree assignment solution

---
+++ TreeAssignment/TreeAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
