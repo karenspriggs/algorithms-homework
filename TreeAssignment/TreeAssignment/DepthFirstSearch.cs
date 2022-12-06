addbfd88643d93e05e1dc53d471d6623fc0e9155
Karen Spriggs <k.spriggs01@gmail.com>
2022-12-06 16:22:24 -0600

    tree assignment implementation

---
+++ TreeAssignment/TreeAssignment/DepthFirstSearch.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeAssignment
+{
+    public class TreeNode
+    {
+        public int Key;
+        public TreeNode LeftNode;
+        public TreeNode RightNode;
+
+        public TreeNode(int _key)
+        {
+            Key = _key;
+            LeftNode = null;
+            RightNode = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        private TreeNode RootNode;
+
+        // Takes values from an array and recursively adds it to the binary tree
+        public TreeNode InsertValueInLevelOrder(int[] data, int index)
+        {
+            TreeNode rootNode = null;
+
+            if (index < data.Length)
+            {
+                rootNode = new TreeNode(data[index]);
+
+                rootNode.LeftNode = InsertValueInLevelOrder(data, 2 * index + 1);
+                rootNode.RightNode = InsertValueInLevelOrder(data, 2 * index + 2);
+            }
+
+            return rootNode;
+        }
+
+        // Sets the node values from the elements in the array and then
+        // prints the tree using the three traversal strategies
+        public BinaryTree(int[] data)
+        {
+            RootNode = InsertValueInLevelOrder(data, 0);
+
+            PostOrderTraversal(RootNode);
+            InOrderTraversal(RootNode);
+            PreOrderTraversal(RootNode);
+        }
+
+        // Postorder traversal is used to delete a binary search tree
+        // or to get the postfix expression of a an expression tree
+        private void PostOrderTraversal(TreeNode currentNode)
+        {
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            PostOrderTraversal(currentNode.LeftNode);
+            PostOrderTraversal(currentNode.RightNode);
+
+            Console.WriteLine(currentNode.Key);
+        }
+
+        // In order traversal gives the nodes in a non-decreasing order
+        private void InOrderTraversal(TreeNode currentNode)
+        {
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            InOrderTraversal(currentNode.LeftNode);
+
+            Console.WriteLine(currentNode.Key);
+
+            InOrderTraversal(currentNode.RightNode);
+        }
+
+        // Pre order traversal is often used to create a copy of a binary
+        // tree, or to get the prefix expression on an expression tree
+        private void PreOrderTraversal(TreeNode currentNode)
+        {
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(currentNode.Key);
+
+            PreOrderTraversal(currentNode.LeftNode);
+            PreOrderTraversal(currentNode.RightNode);
+        }
+    }
+}
--- TreeAssignment/TreeAssignment/Program.cs
+++ TreeAssignment/TreeAssignment/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
 
 namespace TreeAssignment
 {
@@ -6,7 +9,68 @@ namespace TreeAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileReader.fileName = "scores.txt";
+            int[] dataArray = FileReader.ReadDataFromFileToArray();
+
+            SortingAlgorithm.InsertionSort(dataArray);
+
+            new BinaryTree(dataArray);
+        }
+
+        public static class FileReader
+        {
+            public static string fileName;
+
+            public static int[] ReadDataFromFileToArray()
+            {
+                List<int> output = new List<int>();
+                int[] ArrayOutput;
+                int arraySize = 0;
+
+                StreamReader streamReader = new StreamReader(fileName);
+                streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                int element = Convert.ToInt32(streamReader.ReadLine());
+
+                while (element != null)
+                {
+                    output.Add(element);
+                    element = Convert.ToInt32(streamReader.ReadLine());
+                    arraySize++;
+                }
+
+                ArrayOutput = new int[arraySize];
+
+                for (int i = 0; i < arraySize; i++)
+                {
+                    ArrayOutput[i] = output[i];
+                }
+
+                return ArrayOutput;
+            }
+        }
+
+        public static class SortingAlgorithm
+        {
+            public static void InsertionSort(int[] data)
+            {
+                for (int i = 0; i < data.Length - 1; i++)
+                {
+                    for (int j = i - 1; j > 0; j--)
+                    {
+                        if (data[j] < data[j - 1])
+                        {
+                            int tempvalue = data[j];
+                            data[j - 1] = data[j];
+                            data[j] = tempvalue;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
         }
     }
 }
